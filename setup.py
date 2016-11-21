9574fd9da5f9d8a08a6124633c43f13fc76790b1
alexjvan <Dreamonclaids@gmail.com>
2016-11-20 16:16:40 -0800

    Folder Fixes

--- setup.py
+++ setup.py
@@ -6,26 +6,13 @@ LICENSE_TYPE = "Apache"
 
 setup(
     name = "Amy",
-    version = CURRENT_VERSION,
-    packages = find_packages(),
-    scripts = [''],
-
-    install_requires = [''],
-
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'Amy' package, too:
-        'Amy': ['*.msg'],
-    }
-
-    # metadata for upload to PyPI
     author = "Alexander, \'Adeus D. Claid, Aerius L. Anima\' J. Van Matre",
     author_email = "tryamyai@gmail.com",
-    description = "This is a custom built, general-use, all-around artificial intelligence system.",
+    version = CURRENT_VERSION,
+    description = "This is a general-use artificial intelligence system.",
+    long_description = open('README.rst').read(),
+    packages = find_packages(),
+    install_requires = [''],
     license = LICENSE_TYPE,
-    keywords = "Amy ai",
-    url = "",   # project home page, if any
-
-    # could also include long_description, download_url, classifiers, etc.
+    keywords = "Amy AI artificial intelligence Alexander Van Matre Adeus Claid Aerius Anima",
 )
